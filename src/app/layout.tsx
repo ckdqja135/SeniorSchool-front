12f46928e5edf9fd5ff97cc8d7bcb82e8e0caab3
suayeyo <suayeyo@proton.me>
2025-03-08 11:04:55 +0900

    Format(#3): Modify Prettier 추가에 따른 기존 코드 일부 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,11 @@ import "@/styles/globals.css";
 import localFont from "next/font/local";
 
 const pretendard = localFont({
-  src: './fonts/PretendardVariable.woff2',
-  weight: '45 920',
-  style: 'normal',
-  display: 'swap',
-})
+  src: "./fonts/PretendardVariable.woff2",
+  weight: "45 920",
+  style: "normal",
+  display: "swap",
+});
 
 export default function RootLayout({
   children,
@@ -14,11 +14,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ko" className={`${pretendard.className} antialisaed`}>
-      <body
-      >
-        {children}
-      </body>
+    <html lang='ko' className={`${pretendard.className} antialisaed`}>
+      <body>{children}</body>
     </html>
   );
 }
