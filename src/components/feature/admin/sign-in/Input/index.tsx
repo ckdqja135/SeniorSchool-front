745d551509ed6e07e57f240f057c057dd80ccdad
suayeyo <suayeyo@proton.me>
2025-03-09 21:12:56 +0900

    Feat(#5): Add admin sign-in을 위한 컴포넌트 추가

---
+++ src/components/feature/admin/sign-in/Input/index.tsx
@@ -0,0 +1,37 @@
+import CommonInput from "@/components/common/Input";
+import { SignInProps } from "@/types/User";
+
+const SignInInput = ({
+  username,
+  setUsername,
+  password,
+  setPassword,
+  error,
+}: SignInProps) => {
+  const signInInputClassName = `p-1 border-2 rounded-md placeholder:text-gray-300 placeholder:text-medium min-w-12 w-full ${error ? "border-red-500" : "border-gray-300"}`;
+
+  return (
+    <>
+      <CommonInput
+        label='아이디'
+        inputType='text'
+        className={signInInputClassName}
+        placeholder='아이디를 입력해주세요'
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        error={error}
+      />
+      <CommonInput
+        label='비밀번호'
+        inputType='password'
+        className={signInInputClassName}
+        placeholder='******'
+        value={password}
+        onChange={(e: any) => setPassword(e.target.value)}
+        error={error}
+      />
+    </>
+  );
+};
+
+export default SignInInput;
